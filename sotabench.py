7b1a1cd81e5f8dac23bbf5d3eecb1ea0834d968d
Quinn <43769250+Quinn9928@users.noreply.github.com>
2021-07-30 21:26:46 +0800

    Update sotabench.py

--- sotabench.py
+++ sotabench.py
@@ -1,1 +1,11 @@
 from torchbench.image_classification import ImageNet
+evaluator = ImageNetEvaluator(
+    # automatically compare to this paper
+    model_name='ResNeXt-101-32x8d',
+    paper_arxiv_id='1611.05431'
+)
+
+predictions = ... # use your model to make predictions
+
+evaluator.add(predictions)
+evaluator.save()
